3a6871118d6c72137223b8574029f356ef3f7b78
lawrence-innoative-tech <gusantonylaragmail.com>
2024-10-30 20:46:53 +0530

    completed

--- src/main/java/com/data/structure/example/backtracking/OwnNQueenProblem.java
+++ src/main/java/com/data/structure/example/backtracking/OwnNQueenProblem.java
@@ -3,8 +3,8 @@ package com.data.structure.example.backtracking;
 public class OwnNQueenProblem {
 
     public static void main(String[] args) {
-        int n = 4;
-        int backTrackCount = 0, placedQueenCount = 0;
+        int n = 8;
+        int backTrackCount = 0;
         int[] placedQueen = new int[n];
         char[][] board = new char[n][n];
 
@@ -13,7 +13,6 @@ public class OwnNQueenProblem {
                 if (isSafe(board, i, j)) {
                     board[i][j] = 'Q';
                     placedQueen[i] = j;
-                    placedQueenCount++;
                     backTrackCount = 0;
                     break;
                 } else {
@@ -24,7 +23,7 @@ public class OwnNQueenProblem {
                     --i;
 
                     for (int k = i; k >= 0 ; k--) {
-                        if (placedQueen[k] == n) {
+                        if (placedQueen[k] == ( n - 1 )) {
                             board[k][n - 1] = '.';
                             placedQueen[k] = 0;
                         } else {
