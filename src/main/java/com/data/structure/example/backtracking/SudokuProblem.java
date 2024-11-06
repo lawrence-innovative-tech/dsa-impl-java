8ff9287ac21e1122a23f5e4e229e6c69eaffd8c6
lawrence-innoative-tech <gusantonylara@gmail.com>
2024-11-06 19:51:17 +0530

    sudoku completed

--- src/main/java/com/data/structure/example/backtracking/SudokuProblem.java
+++ src/main/java/com/data/structure/example/backtracking/SudokuProblem.java
@@ -15,22 +15,73 @@ public class SudokuProblem {
                 {0, 0, 0, 4, 1, 9, 0, 0, 5},
                 {0, 0, 0, 0, 8, 0, 0, 7, 9}
         };
+        int[][] defaultBoard = new int[sudokuBoard.length][sudokuBoard.length];
 
+        for (int i = 0; i < sudokuBoard.length; i++) {
+            int j = 0, number = 0;
 
+            while (j < sudokuBoard.length) {
+
+                if ( number > sudokuBoard.length) {
+                    j--;
+                    if (j < 0) {
+                        --i;
+                        j = sudokuBoard.length - 1;
+                    }
+                    for (int k = j; k >= -1; k--) {
+                        if (k == -1){
+                            --i;
+                            k = sudokuBoard.length;
+                            continue;
+                        }
+                        if (defaultBoard[i][k] == 0) {
+                            j = k;
+                            break;
+                        }
+                    }
+                    number = sudokuBoard[i][j];
+                    sudokuBoard[i][j] = 0;
+                    number++;
+                    continue;
+                }
+
+                if (sudokuBoard[i][j] == 0){
+                    if (isSafe(sudokuBoard, i, j, number)){
+                        sudokuBoard[i][j] = number;
+                        j++;
+                        number = 0;
+                    }
+                    else {
+                        number++;
+                    }
+                } else {
+                    defaultBoard[i][j] = 1;
+                    j++;
+                }
+            }
+        }
+
+        for (int i = 0; i < sudokuBoard.length; i++) {
+            for (int j = 0; j < sudokuBoard.length; j++){
+                System.out.print(sudokuBoard[i][j] + " ");
+            }
+            System.out.println();
+        }
 
     }
 
-    public static boolean isSafe(int[][] board, int row, int col){
+    public static boolean isSafe(int[][] board, int row, int col, int colNumber){
 
-        if (board[row][col] != 0){
-            return false;
+        for (int i = 0; i < board.length; i++){
+            if (board[i][col] == colNumber){
+                return false;
+            }
         }
 
-        for (int i = 0; i < row; i++){
-            if (board[i][col] == ){
-
+        for (int j = 0; j < board.length; j++){
+            if (board[row][j] == colNumber){
+                return false;
             }
-
         }
 
         return true;
